481dcfeb4b53ae0de9002ede7f50f8ef86f97527
RyanPioneer <ryanpionee2@gmail.com>
2024-01-28 20:10:52 +0800

    2024/1/28

---
+++ Educational Codeforces Round 161 (Rated for Div. 2)/1922E - Increasing Subsequences.cpp
@@ -0,0 +1,56 @@
+/**
+ * Source: jpeg.ly/6P072
+ * Date: 2024/1/28
+ * Skill: constructive algorithms
+ * Constraints:
+ *
+ */
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <numeric>
+#include <tuple>
+#include <stack>
+#include <bitset>
+#include <functional>
+#include <list>
+
+
+#define SZ(X) ((int)(X).size())
+#define ll long long
+using namespace std;
+
+
+void solve() {
+    ll x;
+    cin >> x;
+    x--;
+    vector<int> res;
+    int cur = 200;
+    while (x) {
+        if (x % 2) {
+            res.push_back(cur--);
+            x /= 2;
+        } else {
+            res.push_back(0);
+            x--;
+        }
+    }
+    reverse(begin(res), end(res));
+    cout << SZ(res) << endl;
+    for (auto &i: res) cout << i << " ";
+    cout << endl;
+}
+
+int main() {
+    int t;
+    for (cin >> t; t--;) {
+        solve();
+    }
+}
